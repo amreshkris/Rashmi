93f9e4b259ea23a1f41029bce98ee57a0b28d875
amreshkris <amresh.kris@gmail.com>
2014-08-08 09:37:47 +0530

    Sidebar changes
    Sidebar changes

---
+++ app/app.js
@@ -0,0 +1,37 @@
+'use strict';
+var rashmi = angular.module('rashmi', ['ngRoute', 'ui.bootstrap'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'app/templates/home.html',
+        controller: 'home'
+    });
+    $routeProvider.when('/about', {
+        templateUrl: 'app/templates/about.html',
+        controller: 'about'
+    });
+    $routeProvider.when('/consultants', {
+        templateUrl: 'app/templates/consultants.html',
+        controller: 'about'
+    });
+    $routeProvider.when('/facilities', {
+        templateUrl: 'app/templates/facilities.html',
+        controller: 'about'
+    });
+    $routeProvider.when('/location', {
+        templateUrl: 'app/templates/location.html',
+        controller: 'about'
+    });
+    $routeProvider.when('/gallery', {
+        templateUrl: 'app/templates/gallery.html',
+        controller: 'gallery'
+    });
+    $routeProvider.when('/ent', {
+        templateUrl: 'app/templates/ent.html',
+        controller: 'ent'
+    });
+    $routeProvider.when('/cashless', {
+        templateUrl: 'app/templates/cashless.html',
+        controller: 'about'
+    });
+    $routeProvider.otherwise({ redirectTo: '/home' });
+}]);
